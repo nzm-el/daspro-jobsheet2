0960fbf36e3213ef67e73e9e0d269ff28cc2198d
nzm-el <nizamel1402@gmail.com>
2024-09-04 10:10:41 +0700

    simpan isi kode java ke Github

--- MyFirstJavaGithub18.java
+++ MyFirstJavaGithub18.java
@@ -1,4 +1,4 @@
-public class MyfirstJavaGithub18  {
+public class MyFirstJavaGithub18  {
         public static void main(String[] args)  (
             System.out.println("Hello from GitHUb");
         )
